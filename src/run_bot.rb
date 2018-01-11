cb94f0fe6a1a53cd868d464a7d7f171aa7272cf7
Simon Murphy <simonmurphy@bluebridgetech.com>
2018-01-11 16:21:28 +0000

    Updated storage, updated command centre to return more data. Added file to run bot.

---
+++ src/run_bot.rb
@@ -0,0 +1,20 @@
+require 'discordrb'
+require_relative 'command_centre/command_centre.rb'
+
+token = ARGV[0]
+client_id = ARGV[2]
+
+bot = Discordrb::Bot.new token: token, client_id: client_id
+
+command_centre = CommandCentre.new()
+
+def remove_promt(command)
+  return command.gsub("!", "").downcase.strip
+end
+
+bot.message(with_text: /^!.+/) do |event|
+  command = event.content.to_s()
+  event.respond command_centre.process_command(remove_promt(command))
+end
+
+bot.run
