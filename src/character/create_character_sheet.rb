a39e3fb02bed3e66d4f9c6d84be290bc50075702
Simon Murphy <simonmurphy@bluebridgetech.com>
2018-01-12 23:31:33 +0000

    Added skeleton for creating character sheet.

---
+++ src/character/create_character_sheet.rb
@@ -0,0 +1,53 @@
+class CreateCharacterSheet
+  def name_character()
+    
+  end
+
+  def choose_races()
+
+  end
+
+  def choose_class()
+
+  end
+
+  def choose_skills()
+
+  end
+
+  def choose_cantrips()
+
+  end
+
+  def choose_level_1_spells()
+
+  end
+
+  def choose_archetype()
+
+  end
+
+  def choose_weapon()
+
+  end
+
+  def choose_armour()
+
+  end
+
+  def choose_background()
+
+  end
+
+  def roll_attributes?()
+
+  end
+
+  def custom_attribute()
+
+  end
+
+  def add_discription()
+
+  end
+end
